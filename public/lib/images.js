aa8b449a1cbb71d2f7edcc724e1db0aba2be7cb2
Yuliia Kvitchasta <143713265+YuliiaKvitka@users.noreply.github.com>
2024-06-22 12:43:54 +0300

    Update images.js

--- public/lib/images.js
+++ public/lib/images.js
@@ -5,8 +5,15 @@ import second from '../assets/slider/4.jpg'
 
 
 export const images = [
-  { src: first, alt: 'First' },
-  { src: second, alt: 'Second' },
+  {
+    src: first,
+    alt: 'First',
+    blurDataURL: 'data:image/jpeg;base64,...' // Генерируемый URL для эффекта blur
+  },
+  {
+    src: second,
+    alt: 'Second',
+    blurDataURL: 'data:image/jpeg;base64,...' // Генерируемый URL для эффекта blur
+  },
   
- 
 ]
